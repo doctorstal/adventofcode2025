a25f5a26197572c6b8cc78317240a0a8e3a20b22
Oleksii Stadnyk <doctorstal@gmail.com>
2025-12-03 10:33:49 +0100

    Day3 pt2

--- solutions/3.go
+++ solutions/3.go
@@ -1,6 +1,9 @@
 package solutions
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func ThirdDay() error {
 	fmt.Println("--- Day 3: Lobby ---")
@@ -12,31 +15,48 @@ func ThirdDay() error {
 	}
 
 	totalJoltage := 0
+	totalJoltagePt2 := 0
 	for _, bank := range banks {
 		totalJoltage += findMaxJoltageInBank(bank)
+		totalJoltagePt2 += findMaxJoltageInBankPt2(bank)
 	}
 
 	fmt.Printf("Answer: %d\n", totalJoltage)
+	fmt.Printf("Answer Pt2: %d\n", totalJoltagePt2)
 
 	return nil
 }
 
 func findMaxJoltageInBank(bank string) int {
+	currPos := 0
+	res := 0
+	for i := 2; i >= 1; i -= 1 {
+		pos := posMaxDigitInBank(bank, currPos, len(bank)-i+1)
+		currPos = pos + 1
+		res += int(bank[pos]-'0') * int(math.Pow10(i-1))
+	}
+	return res
+}
+
+func findMaxJoltageInBankPt2(bank string) int {
+	currPos := 0
+	res := 0
+	for i := 12; i >= 1; i -= 1 {
+		pos := posMaxDigitInBank(bank, currPos, len(bank)-i+1)
+		currPos = pos + 1
+		res += int(bank[pos]-'0') * int(math.Pow10(i-1))
+	}
+	return res
+}
+
+func posMaxDigitInBank(bank string, start, end int) int {
 	var m1 byte = 0
 	m1Pos := 0
-	for i := range len(bank) - 1 {
+	for i := start; i < end; i++ {
 		if bank[i] > m1 {
 			m1 = bank[i]
 			m1Pos = i
 		}
 	}
-	var m2 byte = 0
-	for i := m1Pos + 1; i < len(bank); i += 1 {
-		if bank[i] > m2 {
-			m2 = bank[i]
-		}
-	}
-	// fmt.Printf("m1: %v, m2: %v\n", m1-'0', m2-'0')
-
-	return int((m1-'0')*10 + (m2 - '0'))
+	return m1Pos
 }
